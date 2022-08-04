0f64733288d7d110f7c4201e6d63e4e0a7986d65
John Paul <johnpaul@trees.local>
2022-08-04 10:21:33 +0300

    connect to db succesfully

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const cors = require("cors");
 
+// help us connect to the database
+const mongoose = require("mongoose");
+
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: false });
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
